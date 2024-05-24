43c981a0ce0ed1b8fa10cb40f578b955e4b62821
Tello <pateltanaka22@live.com>
2024-05-24 12:05:16 +0200

    create city  method

---
+++ src/services/cities.services.ts
@@ -0,0 +1,21 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cities } from 'src/models';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TransactionService {
+  constructor(
+    @InjectRepository(Cities)
+    private cityModel: Repository<Cities>,
+  ) {}
+
+  async createCity(cityInfo: any): Promise<any> {
+    try {
+      const newCity = await this.cityModel.create(cityInfo);
+      await this.cityModel.save(newCity);
+    } catch (error) {
+      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
