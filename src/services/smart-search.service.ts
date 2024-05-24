46b98e51e20a94a6e8ed4c6d5d2a462d829c6681
Tello <pateltanaka22@live.com>
2024-05-24 18:54:39 +0200

    smart search

--- src/services/smart-search.service.ts
+++ src/services/smart-search.service.ts
@@ -2,12 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Brands, Cities, Diets, DishTypes } from 'src/models';
 import { Connection, Repository } from 'typeorm';
-import {
-  BrandsService,
-  CitiesService,
-  DietsService,
-  DishTypesService,
-} from '.';
 
 @Injectable()
 export class SmartSearchService {
@@ -19,65 +13,8 @@ export class SmartSearchService {
     @InjectRepository(Brands)
     private brandsModel: Repository<Brands>,
     @InjectRepository(Cities)
-    private cityModel: Repository<Cities>,
-    private brandsService: BrandsService,
-    private citiesService: CitiesService,
-    private dietsService: DietsService,
-    private dishTypesService: DishTypesService,
     private connection: Connection,
   ) {}
-
-  async searchEntities(
-    searchTerm: string,
-    page: number = 1,
-    pageSize: number = 10,
-  ) {
-    const entities = [];
-    const entityTypes = {
-      city: 'cities',
-      brand: 'brands',
-      dishType: 'dishTypes',
-      diet: 'diets',
-    };
-
-    for (const entityType in entityTypes) {
-      const keyword = entityTypes[entityType];
-      const matches = await this.findMatchingEntities(
-        searchTerm,
-        keyword,
-        page,
-        pageSize,
-      );
-
-      if (matches.length > 0) {
-        for (const match of matches) {
-          entities.push({ [entityType]: match });
-        }
-      }
-    }
-
-    return entities;
-  }
-
-  // Helper function for fetching and filtering entities of a specific type
-  async findMatchingEntities(
-    searchTerm: string,
-    keyword: string,
-    page?: number,
-    pageSize?: number,
-  ) {
-    const terms = searchTerm.toLowerCase().split(' ');
-    const regexPattern = new RegExp(
-      terms.map((term) => `\\b${term}\\w*`).join('|'),
-    );
-
-    const matchingEntities = await this[`${keyword}Service`].findAll(); // Access service dynamically
-    const resultAfterFilter = matchingEntities.filter((entity) =>
-      regexPattern.test(entity.name.toLowerCase()),
-    );
-
-    return resultAfterFilter;
-  }
 
   async extractEntities(
     searchTerm: string,
